b4a74e3a698228479a1ddb206f2236956f210b85
shellyrgarner <sgrenea@gmail.com>
2018-09-20 11:30:44 -0400

    updated mongo db to mongoheadlines

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -10,7 +10,7 @@ var db = require("../models");
 
 mongoose.connect("mongodb://localhost/mongoNews", { useNewUrlParser: true });
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoNews";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
